a55c54b154fe0c1bba6478ff50dafebdbc85a782
bezaluv <teklesenbet.filmon@yahoo.com>
2019-11-22 01:11:28 -0500

    lesson 7

--- app/src/main/java/com/milleb/swoosh/Controller/FinishActivity.kt
+++ app/src/main/java/com/milleb/swoosh/Controller/FinishActivity.kt
@@ -2,9 +2,9 @@ package com.milleb.swoosh.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.milleb.swoosh.Model.Player
 import com.milleb.swoosh.R
-import com.milleb.swoosh.Utilities.EXTRA_LEAGUE
-import com.milleb.swoosh.Utilities.EXTRA_SKILL
+import com.milleb.swoosh.Utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_finish.*
 
 class FinishActivity : AppCompatActivity() {
@@ -12,9 +12,8 @@ class FinishActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish)
-        val league = intent.getStringExtra(EXTRA_LEAGUE)
-        val skill = intent.getStringExtra(EXTRA_SKILL)
 
-        searchLeaguesText.text = "Looking for $league $skill league near you..."
+        val player = intent.getParcelableExtra<Player>(EXTRA_PLAYER)
+        searchLeaguesText.text = "Looking for ${player.league} ${player.skill} league near you..."
     }
 }
--- app/src/main/java/com/milleb/swoosh/Controller/LeagueActivity.kt
+++ app/src/main/java/com/milleb/swoosh/Controller/LeagueActivity.kt
@@ -4,13 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import com.milleb.swoosh.Utilities.EXTRA_LEAGUE
+import com.milleb.swoosh.Model.Player
 import com.milleb.swoosh.R
+import com.milleb.swoosh.Utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
 
-    var selectedLeague = ""
+
+    var player = Player(league = "", skill = "")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,14 +24,14 @@ class LeagueActivity : BaseActivity() {
         womensLeagueBtn.isChecked = false
         coedLeagueBtn.isChecked = false
 
-        selectedLeague = "mens"
+        player.league = "mens"
     }
 
     fun onWomensClicked(view: View){
         mensLeagueBtn.isChecked = false
         coedLeagueBtn.isChecked = false
 
-        selectedLeague = "womens"
+        player.league = "womens"
 
     }
 
@@ -37,14 +39,14 @@ class LeagueActivity : BaseActivity() {
         mensLeagueBtn.isChecked = false
         womensLeagueBtn.isChecked = false
 
-        selectedLeague = "co-ed"
+        player.league = "co-ed"
 
     }
 
     fun leagueNextClicked(view: View){
-        if (selectedLeague != ""){
+        if (player.league != ""){
             val skillActivity = Intent(this, SkillActivity::class.java)
-            skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+            skillActivity.putExtra(EXTRA_PLAYER, player)
             startActivity(skillActivity)
 
         } else {
